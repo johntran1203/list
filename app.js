e79ce97e16a76c8c434b8ed8d1f2a2e8a02c1090
johntran <tranjohn1211@gmail.com>
2023-04-30 22:04:46 -0700

    finished link list problem

--- app.js
+++ app.js
@@ -1,37 +1,53 @@
-// Implement a search method in the linkedList Class that takes in a value to be searched. You method should return a boolean. 
+// Implement a search method in the linkedList Class that takes in a value to be searched. You method should return a boolean.
 // Include testing code in your submisison
 
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class LinkedList {
-    constructor() {
-      this.head = null;
-      this.size = 0;
-    }
-  }
-prepend(data) {
-    const n = new Node(data);
-    if(this.size === 0) {
-        this.header =n;
-        this.tail = n;
-    } else{
-        n.next = this.header;
-        this.header =n;
-    }
-    this.size++
 }
 
-printList() {
-    let data = "";
-    let current = this.header;
-    while(current !=null) {
-        data = data + current.data +"";
-        current = current.next
-    }
-    return data
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 }
+
+LinkedList.prototype.add = function (value) {
+  const newNode = new Node(value);
+
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+  this,this.size++
+};
+
+LinkedList.prototype.search = function (value){
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      
+      current = current.next;
+    }
+
+    return false;
+  }
+
+
+  const list = new LinkedList();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+
+  console.log(list.search(3));
