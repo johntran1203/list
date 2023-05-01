94260389462446e9c2da724399abe996a548142f
johntran <tranjohn1211@gmail.com>
2023-04-30 21:17:30 -0700

    ned linked list

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // Implement a search method in the linkedList Class that takes in a value to be searched. You method should return a boolean. 
 // Include testing code in your submisison
 
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
+    }
+  }
 prepend(data) {
     const n = new Node(data);
     if(this.size === 0) {
@@ -11,4 +24,14 @@ prepend(data) {
         this.header =n;
     }
     this.size++
+}
+
+printList() {
+    let data = "";
+    let current = this.header;
+    while(current !=null) {
+        data = data + current.data +"";
+        current = current.next
+    }
+    return data
 }
