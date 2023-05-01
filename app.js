7469baad16c99d12abbc08eddb42df7c9b0bd809
johntran <tranjohn1211@gmail.com>
2023-04-30 20:56:17 -0700

    prep data

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // Implement a search method in the linkedList Class that takes in a value to be searched. You method should return a boolean. 
 // Include testing code in your submisison
 
-console.log('test')
+prepend(data) {
+    const n = new Node(data);
+    if(this.size === 0) {
+        this.header =n;
+        this.tail = n;
+    } else{
+        n.next = this.header;
+        this.header =n;
+    }
+    this.size++
+}
