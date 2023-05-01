dc337bdbf0ac2075c9105017921f8ad50ff68a93
johntran <tranjohn1211@gmail.com>
2023-04-30 20:48:33 -0700

    first commit question

---
+++ app.js
@@ -0,0 +1,4 @@
+// Implement a search method in the linkedList Class that takes in a value to be searched. You method should return a boolean. 
+// Include testing code in your submisison
+
+console.log('test')
